25e9f1bb32810bf1eff571cb4a89b17bf3f46ba6
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-03-05 20:23:45 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -57,7 +57,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",style=enums.ButtonStyle.PRIMARY
+                callback_data="GetTimer",
             )
         ],
         [
